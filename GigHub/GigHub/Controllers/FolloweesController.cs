90d8ba542c05c358fda7d5a7d2ffacc0b67b8908
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-20 15:24:49 -0400

    Implement Artists I'm Following

---
+++ GigHub/GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = ApplicationDbContext.Create();
+        }
+
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var followees = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Include(f => f.Artist)
+                .ToList();
+
+            return View(followees);
+        }
+    }
+}
