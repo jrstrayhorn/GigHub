559af1c356c2c2960803185b5eb304a1522f57e9
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-20 11:43:46 -0400

    Working on Add Gig to User's Calendar use case

--- GigHub/GigHub/Models/ApplicationDbContext.cs
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,18 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // add this will turn off multiple cascading deletes
+            // so if Gig is deleted it won't delete attendance records
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
