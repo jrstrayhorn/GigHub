85890ff6a577983fe0a5c7c853fc1bb4ecd14a63
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-26 09:27:00 -0400

    Extracting Interfaces for Repositories and IUnitOfWork, implementing Dependency Injection w/ Ninject

---
+++ GigHub/GigHub/Repositories/IAttendanceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.Repositories
+{
+    public interface IAttendanceRepository
+    {
+        Attendance GetAttendance(string userId, int gigId);
+        IEnumerable<Attendance> GetFutureAttendances(string userId);
+    }
+}
